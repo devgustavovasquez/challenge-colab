c82f28ba8561b16b1664407a7e08268794992e0a
Gustavo Vasquez <gustavovasquez2002@gmail.com>
2023-04-11 05:59:31 -0300

    chore: add antd drivers

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,10 @@
+import { Button } from "antd";
+
 export default function Home() {
-  return <h1>Hello World</h1>;
+  return (
+    <>
+      <h1>Hello World</h1>
+      <Button type="default">Button</Button>
+    </>
+  );
 }
