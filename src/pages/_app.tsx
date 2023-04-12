75e5e3283cd8e48740e7353f1465163617ed950c
Gustavo Vasquez <gustavovasquez2002@gmail.com>
2023-04-12 09:45:06 -0300

    feat: change antd theme

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,18 @@
 import "@/styles/globals.css";
+import { ConfigProvider } from "antd";
 import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <ConfigProvider
+      theme={{
+        token: {
+          colorPrimary: "#7e22ce",
+          colorSplit: "#7e22ce32",
+        },
+      }}
+    >
+      <Component {...pageProps} />
+    </ConfigProvider>
+  );
 }
