744282038bcf7392ed635f2eaed6643e2d48cbde
Gustavo Vasquez <gustavovasquez2002@gmail.com>
2023-04-12 10:38:15 -0300

    feat: add number of users functionality

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,27 +5,39 @@ import Image from "next/image";
 
 export default function Home() {
   const [data, setData] = useState<APIResponse>();
+  const [offset, setOffset] = useState<number | null>(10);
 
   useEffect(() => {
-    fetch("https://randomuser.me/api/?results=10")
+    if (!offset) return setData(undefined);
+
+    fetch(`https://randomuser.me/api/?results=${offset}`)
       .then((response) => response.json())
       .then((data) => setData(data));
 
     return () => {
       setData(undefined);
     };
-  }, []);
+  }, [offset]);
+
+  const handleOffsetChange = (value: number | null) => {
+    setOffset(value);
+  };
 
   return (
-    <section className="flex flex-col items-center justify-center max-w-5xl m-auto">
+    <section className="h-full flex flex-col items-center justify-center max-w-5xl m-auto">
       <h1 className="text-4xl font-semibold text-center my-8 text-zinc-800">
         Lista de Usuários
       </h1>
       <div className="flex items-center gap-2">
-        <label className="cursor-pointer py-2 px-1" htmlFor="users">
+        <label className="cursor-pointer py-2 px-1" htmlFor="offset">
           Quantidade de usuários:
         </label>
-        <InputNumber defaultValue={10} id="users" />
+        <InputNumber
+          id="offset"
+          onChange={handleOffsetChange}
+          defaultValue={10}
+          min={1}
+        />
       </div>
       {data && (
         <List
@@ -34,7 +46,6 @@ export default function Home() {
           dataSource={data.results}
           renderItem={(item) => (
             <List.Item
-              className="border-2 border-gray-200 rounded-lg p-4"
               actions={[
                 <Button type="default" key={`link-${item.login.uuid}`}>
                   Ver mais
